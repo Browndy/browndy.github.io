ff084a5d826d2b0aa50d0d5b109440d2a1224de1
Browndy <57893396+Browndy@users.noreply.github.com>
2024-06-04 15:08:36 +0200

    Update TWLineDrawer.user.js
    fixed user script

--- DieStämme-Stuff/TW Line Drawer/TWLineDrawer.user.js
+++ DieStämme-Stuff/TW Line Drawer/TWLineDrawer.user.js
@@ -16,41 +16,42 @@
 	const win = typeof unsafeWindow != 'undefined' ? unsafeWindow : window;
 
 	// show UI and enable line mode
-	const enableScriptHotkey = win.TWLD_enableScriptHotkey || "l";
+	win.TWLD_enableScriptHotkey || "l";
 	// enable line mode
-	const enableLineModeHotkey = win.TWLD_enableLineModeHotkey || "q";
+	win.TWLD_enableLineModeHotkey || "q";
 	// enable label mode
-	const enableLabelModeHotkey = win.TWLD_enableLabelModeHotkey || "r";
+	win.TWLD_enableLabelModeHotkey || "r";
 	// enable delete mode
-	const enableDeleteModeHotkey = win.TWLD_enableDeleteModeHotkey || "t";
+	win.TWLD_enableDeleteModeHotkey || "t";
 	// enable select corridor mode
-	const enableSelectCorridorModeHotkey = win.TWLD_enableSelectCorridorHotkey || "u";
+	win.TWLD_enableSelectCorridorHotkey || "u";
 	// enable edit select mode
-	const enableEditSelectModeHotkey = win.TWLD_enableEditSelectModeHotkey || "i";
+	win.TWLD_enableEditSelectModeHotkey || "i";
 	// adds the drawn line to the selected group
-	const addLineHotkey = win.TWLD_addLineHotkey || "w";
+	win.TWLD_addLineHotkey || "w";
 	// removes the last coordinates pair
-	const undoCoordsHotkey = win.TWLD_undoCoordsHotkey || "e";
+	win.TWLD_undoCoordsHotkey || "e";
 
 	// set language
-	const lang = win.TWLD_lang || "de";
+	win.TWLD_lang || "de";
 
 	// set default map scale
-	const scale = win.TWLD_scale || 1;
+	win.TWLD_scale || 1;
 
 	// enables zooming for the map
-	const enableZoom = win.TWLD_enableZoom || false;
+	win.TWLD_enableZoom || false;
 
 	// how many continents are populated? (needed for coord wrap)
-	const continents = win.TWLD_continents || 16;
+	win.TWLD_continents || 16;
 
 	// import data from website
-	const data = win.TWLD_data || [
+	win.TWLD_data || [
 		// [world, link]
 		// ["de197", "https://google.de"]
 		// ["de226", "https://browndy.github.io/DieStämme-Stuff/TW%20Line%20Drawer/Test/test.js"],
 		[],
 	];
 
-	$.getScript("https://browndy.github.io/DieStämme-Stuff/TW%20Line%20Drawer/Clean/TWLineDrawer.js");
+	win.$.ajaxSetup({cache = true});
+	win.$.getScript("https://browndy.github.io/DieStämme-Stuff/TW%20Line%20Drawer/Clean/TWLineDrawer.js");
 })();
